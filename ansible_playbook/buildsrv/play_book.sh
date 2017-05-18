2f0f64fb375272e79dd3e38fafd469c963a168c6
aidkit <aidkit.sns@gmail.com>
2017-05-18 19:45:07 +0900

    Jenkins プラグインまでのプレイブック
    プロキシ設定に注意。。

--- ansible_playbook/buildsrv/play_book.sh
+++ ansible_playbook/buildsrv/play_book.sh
@@ -9,12 +9,12 @@ if [ -z "$1" ]; then
     echo "Required playbook file path argument"
 fi
 
-if [ -z "$3" ]; then
+if [ -z "$2" ]; then
     echo "no tags argument"
-    ansible-playbook $1 $2
+    ansible-playbook $1 $3
 
 else
-    ansible-playbook $1 $2 --tags="$3"
+    ansible-playbook $1 $3 --tags="$2"
 fi
 
 
